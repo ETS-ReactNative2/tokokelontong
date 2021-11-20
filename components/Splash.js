8fe5fbeb711f1098b943518f215e8ca2e9527d21
andikscript <andikscript@gmail.com>
2021-11-20 11:07:51 +0700

    add splash screen

---
+++ components/Splash.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import {Image, StyleSheet, Text, View} from 'react-native';
+
+const SplashScreen = () => {
+    return (
+      <View style={styles.container}>
+        <Image source={require('../src/image/logosplash.png')} style={styles.image} />
+        <Text style={styles.text}>Toko</Text>
+        <Text style={styles.text}>Kelontong</Text>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#acd792',
+  },
+  image: {
+    height: 130,
+    width: 170,
+    marginBottom: 10,
+  },
+  text: {
+    fontSize: 22,
+    color: '#fff',
+    flexDirection: 'column',
+    fontFamily: 'ubuntu',
+    fontWeight: 'bold',
+  },
+});
+
+export default SplashScreen;
