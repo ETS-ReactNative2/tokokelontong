dc2596cbe8350edf3c566288895ac30306d9ff91
andikscript <andikscript@gmail.com>
2021-11-20 11:12:45 +0700

    add status bar on splash screen

--- components/Splash.js
+++ components/Splash.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Image, StatusBar, StyleSheet, Text, View} from 'react-native';
 
 const SplashScreen = () => {
     return (
       <View style={styles.container}>
-        <Image source={require('../src/image/logosplash.png')} style={styles.image} />
+        <StatusBar backgroundColor="#acd792" barStyle='light-content' hidden={true} />
+        <Image
+          source={require('../src/image/logosplash.png')}
+          style={styles.image}
+        />
         <Text style={styles.text}>Toko</Text>
         <Text style={styles.text}>Kelontong</Text>
       </View>
