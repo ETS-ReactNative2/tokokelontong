a49ae8ef8356618582afd6e4ee37fcdd6b4a26a9
andikscript <andikscript@gmail.com>
2021-11-23 21:27:15 +0700

    add tab bar button

--- components/BottomTab.js
+++ components/BottomTab.js
@@ -1,82 +1,111 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import React from 'react';
+import React, { createContext, useEffect, useState } from 'react';
 import { Cart } from './Cart/Cart';
 import { Home } from './home/Home';
 import { Orders } from './Orders/Orders';
 import { Recipe } from './Recipe/Recipe';
 import { User } from './User/User';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { BackHandler } from 'react-native';
+import { ButtonTab } from './ButtonTab';
 
 const Tab = createBottomTabNavigator();
+export const colorContext = createContext({
+  colorBackground: 'first',
+  setColorBackground: () => {}
+})
 
 export const BottomTab = () => {
+  const [colorBackground, setColorBackground] = useState('first');
+  const changeValue = {colorBackground, setColorBackground};
+
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      setColorBackground('first');
+    });
+  });
+
     return (
-      <Tab.Navigator
-        screenOptions={{
-          headerShown: false,
-          tabBarStyle: {
-            backgroundColor: '#acd792',
-            height: 60,
-          },
-          tabBarIconStyle: {
-            marginTop: 5,
-          },
-          tabBarInactiveTintColor: '#656565',
-          tabBarActiveTintColor: '#fff',
-          tabBarLabelStyle: {
-            marginBottom: 5,
-            fontFamily: 'Aesthet Nova Regular',
-            fontSize: 12,
-          },
-          tabBarItemStyle: {
-            backgroundColor: '#acd792',
-          },
-        }}>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="home-variant-outline" size={28} color={color} />
-            ),
+      <colorContext.Provider value={changeValue}>
+        <Tab.Navigator
+          screenOptions={{
+            headerShown: false,
+            tabBarStyle: {
+              height: 60,
+              backgroundColor: '#acd792',
+            },
+            tabBarIconStyle: {
+              marginTop: 5,
+            },
+            tabBarInactiveTintColor: '#656565',
+            tabBarActiveTintColor: '#fff',
           }}
-        />
-        <Tab.Screen
-          name="Recipe"
-          component={Recipe}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="book-outline" size={28} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Cart"
-          component={Cart}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="cart-outline" size={28} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Orders"
-          component={Orders}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="shopping-outline" size={28} color={color} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="User"
-          component={User}
-          options={{
-            tabBarIcon: ({color}) => (
-              <Icon name="account-outline" size={28} color={color} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+          >
+          <Tab.Screen
+            name="Home"
+            component={Home}
+            options={{
+              tabBarButton: () => (
+                <ButtonTab
+                  navigations={'Home'}
+                  icon={'home-variant-outline'}
+                  position={'first'}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Recipe"
+            component={Recipe}
+            options={{
+              tabBarButton: () => (
+                <ButtonTab
+                  navigations={'Recipe'}
+                  icon={'book-outline'}
+                  position={'second'}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Cart"
+            component={Cart}
+            options={{
+              tabBarButton: () => (
+                <ButtonTab
+                  navigations={'Cart'}
+                  icon={'cart-outline'}
+                  position={'third'}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="Orders"
+            component={Orders}
+            options={{
+              tabBarButton: () => (
+                <ButtonTab
+                  navigations={'Orders'}
+                  icon={'shopping-outline'}
+                  position={'fourth'}
+                />
+              ),
+            }}
+          />
+          <Tab.Screen
+            name="User"
+            component={User}
+            options={{
+              tabBarButton: () => (
+                <ButtonTab
+                  navigations={'User'}
+                  icon={'account-outline'}
+                  position={'fifth'}
+                />
+              ),
+            }}
+          />
+        </Tab.Navigator>
+      </colorContext.Provider>
     );
 }
