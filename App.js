39f379c540288d483faf7acc75b400a5815fc3f3
andikscript <andikscript@gmail.com>
2021-12-03 23:05:53 +0700

    release apk user 1

--- App.js
+++ App.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import SplashScreen from './components/Splash';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {BottomTab} from './components/BottomTab';
+import {BottomTab, colorContext} from './components/BottomTab';
+import { DetailRecipe } from './components/Recipe/DetailRecipe';
+import { BackHandler } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      console.log('back dari App');
+    });
+    return () => {
+      BackHandler.removeEventListener();
+    };
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -15,6 +26,7 @@ const App = () => {
         }}>
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="BottomTab" component={BottomTab} />
+        <Stack.Screen name="DetailRecipe" component={DetailRecipe} />
       </Stack.Navigator>
     </NavigationContainer>
   );
