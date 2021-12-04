83fa6f8b2d4a9ce38abfc4e03c65daa6036c3853
andikscript <andikscript@gmail.com>
2021-12-05 00:26:52 +0700

    change and fix shadow header and body on home

--- components/home/Body.js
+++ components/home/Body.js
@@ -88,69 +88,78 @@ const renderItem = ({item}) => {
 };
 
 const Item = ({item}) => (
-    <View
+  <View
+    style={{
+      backgroundColor: '#fff',
+      paddingHorizontal: 15,
+      paddingTop: 15,
+      paddingBottom: 10,
+      borderRadius: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 10,
+      marginLeft: 1,
+      elevation: 4,
+      shadowColor: 'green',
+      shadowOffset: {
+        width: 5,
+        height: 10,
+      },
+      shadowOpacity: 1,
+      shadowRadius: 50,
+      borderWidth: 0.3,
+      borderColor: '#acd792',
+      marginEnd: 19,
+    }}>
+    <Image
       style={{
-        backgroundColor: '#fff',
-        paddingHorizontal: 15,
-        paddingTop: 15,
-        paddingBottom: 10,
-        borderRadius: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginBottom: 10,
-        marginLeft: 1,
-        elevation: 4,
-        marginEnd: 19
+        height: 100,
+        width: 110,
+      }}
+      source={item.image}
+    />
+    <Text
+      style={{
+        marginTop: 10,
+        fontSize: 14,
+        fontFamily: 'Aesthet Nova Regular',
+        color: '#656565',
       }}>
-      <Image
+      {item.title}
+    </Text>
+    <Text
+      style={{
+        marginTop: 3,
+        fontSize: 12,
+        fontFamily: 'Aesthet Nova Regular',
+        color: '#656565',
+      }}>
+      {item.harga}
+    </Text>
+    {item.new === '1' ? (
+      <View
         style={{
-          height: 100,
-          width: 110,
-        }}
-        source={item.image}
-      />
-      <Text
-        style={{
-          marginTop: 10,
-          fontSize: 14,
-          fontFamily: 'Aesthet Nova Regular',
-          color: '#656565',
+          position: 'absolute',
+          backgroundColor: '#f44336',
+          width: 40,
+          top: 0,
+          right: 0,
+          borderRadius: 5,
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 5,
         }}>
-        {item.title}
-      </Text>
-      <Text
-        style={{
-          marginTop: 3,
-          fontSize: 12,
-          fontFamily: 'Aesthet Nova Regular',
-          color: '#656565',
-        }}>
-        {item.harga}
-      </Text>
-      {item.new === '1' ? (
-        <View
+        <Text
           style={{
-            position: 'absolute',
-            backgroundColor: '#f44336',
-            width: 40,
-            top: 0,
-            right: 0,
-            borderRadius: 5,
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 5,
+            fontFamily: 'Aesthet Nova Bold',
+            color: '#fff',
+            fontSize: 10,
           }}>
-          <Text
-            style={{
-              fontFamily: 'Aesthet Nova Bold',
-              color: '#fff',
-              fontSize: 10,
-            }}>
-            New
-          </Text>
-        </View>
-      ) : (
-        <View />
-      )}
-    </View>
+          New
+        </Text>
+      </View>
+    ) : (
+      <View />
+    )}
+  </View>
 );
--- components/home/Header.js
+++ components/home/Header.js
@@ -60,11 +60,20 @@ const Item = ({item}) => (
       paddingTop: 15,
       paddingBottom: 10,
       borderRadius: 10,
-      elevation: 3,
+      elevation: 4,
+      shadowColor: 'green',
+      shadowOffset: {
+        width: 5,
+        height: 10,
+      },
+      shadowOpacity: 1,
+      shadowRadius: 50,
       justifyContent: 'center',
       alignItems: 'center',
       marginBottom: 5,
       marginLeft: 1,
+      borderWidth: 0.3,
+      borderColor: '#acd792',
     }}>
     <Image
       style={{
