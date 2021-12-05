a6676af529ac998245a4cec9d8b624e402381ba7
andikscript <andikscript@gmail.com>
2021-12-05 16:53:07 +0700

    add color palette

---
+++ src/colors.js
@@ -0,0 +1,8 @@
+export const colors = {
+    grannySmithApple: '#ACD792',
+    forestGreenCrayolan: '#61A56C',
+    seaGreen: '#3C8C59',
+    darkSpringGreen: '#167346',
+    white: '#FFFFFF',
+    dimGray: '#656565'
+}
