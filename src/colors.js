8c199390e4af9c29cb37565b9047b4d8cacd9819
andikscript <andikscript@gmail.com>
2021-12-05 21:17:31 +0700

    instal library timeline flatlist

--- src/colors.js
+++
@@ -1,8 +0,0 @@
-export const colors = {
-    grannySmithApple: '#ACD792',
-    forestGreenCrayolan: '#61A56C',
-    seaGreen: '#3C8C59',
-    darkSpringGreen: '#167346',
-    white: '#FFFFFF',
-    dimGray: '#656565'
-}
