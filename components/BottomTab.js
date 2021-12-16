0656a55331f689c6d9ed3b7f298ff9db59d73993
andikscript <andikscript@gmail.com>
2021-12-16 21:33:50 +0700

    refactor clean code

--- components/BottomTab.js
+++ components/BottomTab.js
@@ -1,12 +1,12 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { createContext, useState } from 'react';
-import { Home } from './home/Home';
 import Orders from './Orders/Orders';
 import { User } from './User/User';
 import { Recipe } from './Recipe/Recipe';
 import { colors } from '../src/config/colors';
 import { ButtonTab } from '.';
 import { Cart } from './Cart';
+import { Home } from './Dashboard'
 
 const Tab = createBottomTabNavigator();
 export const colorContext = createContext({
