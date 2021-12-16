d1e8082934057723f529bef7690c288ea0a9b3f7
andikscript <andikscript@gmail.com>
2021-12-16 23:00:29 +0700

    add component header for all

---
+++ components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {colors} from '../src/config/colors';
+
+const Header = ({text}) => {
+  return (
+    <View style={styles.containerHeader}>
+      <Text style={styles.textHeader}>{text}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerHeader: {
+    backgroundColor: colors.forestGreenCrayolan,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textHeader: {
+    fontSize: 19,
+    fontFamily: 'Aesthet Nova Regular',
+    color: colors.white,
+  },
+});
+
+export default Header;
