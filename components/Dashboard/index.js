6d8521652a29d32bae465d714ae1110571b915f0
andikscript <andikscript@gmail.com>
2021-12-17 23:28:07 +0700

    add Render Item

--- components/Dashboard/index.js
+++ components/Dashboard/index.js
@@ -3,11 +3,13 @@ import RenderHeader from "./Header";
 import RenderBody from "./Body";
 import RenderFooter from "./Footer";
 import Home from "./Home";
+import ItemBody from "./ItemBody";
 
 export {
     RenderSearch,
     RenderHeader,
     RenderBody,
     RenderFooter,
-    Home
+    Home,
+    ItemBody
 }
