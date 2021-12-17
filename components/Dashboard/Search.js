30cdee7b41c81e920b50b7c8c0fdf83258da938a
andikscript <andikscript@gmail.com>
2021-12-17 21:12:47 +0700

    refactor code styles

--- components/Dashboard/Search.js
+++ components/Dashboard/Search.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-import { Dimensions, Text, TextInput, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, TextInput, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { COLORS } from '../../constants';
 
 const RenderSearch = () => {
     const [search, setSearch] = useState('');
@@ -12,49 +13,13 @@ const RenderSearch = () => {
     };
 
     return (
-      <View
-        style={{
-          position: 'absolute',
-          top: 20,
-          width: Dimensions.get('screen').width
-        }}>
-        <View
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginHorizontal: 20,
-          }}>
-          <Text
-            style={{
-              color: '#fff',
-              fontSize: 30,
-              fontFamily: 'Aesthet Nova Black',
-            }}>
-            Toko Kelontong
-          </Text>
-          <Icon name="map-marker" size={25} color="#fff" />
+      <View style={styles.containerSearch}>
+        <View style={styles.boxTitle}>
+          <Text style={styles.title}>Toko Kelontong</Text>
+          <Icon name="map-marker" size={25} color={COLORS.white} />
         </View>
-        <View
-          onLayout={onLayout}
-          style={{
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center',
-            marginTop: 15,
-            height: 40,
-            marginHorizontal: 20,
-          }}>
-          <View
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center',
-              backgroundColor: '#fff',
-              borderRadius: 10,
-              paddingHorizontal: 10,
-              flex: 1,
-              marginRight: 20,
-            }}>
+        <View onLayout={onLayout} style={styles.containerBoxSearch}>
+          <View style={styles.boxSearch}>
             <Icon name="magnify" size={25} color="#656565" />
             <TextInput
               value={search}
@@ -62,29 +27,64 @@ const RenderSearch = () => {
               style={{}}
               placeholder="Cari barang"
               placeholderTextColor="#656565"
-              style={{
-                marginLeft: 5,
-                fontFamily: 'Aesthet Nova Regular',
-                flex: 1,
-                height: 40,
-                color: '#656565',
-              }}
+              style={styles.search}
             />
           </View>
-          <View
-            style={{
-              backgroundColor: '#fff',
-              width: parentHeight,
-              height: parentHeight,
-              justifyContent: 'center',
-              alignItems: 'center',
-              borderRadius: 10,
-            }}>
+          <View style={[styles.boxCamera, {width: parentHeight, height: parentHeight}]}>
             <Icon name="camera" size={20} color="#656565" />
           </View>
         </View>
       </View>
     );
 }
+
+const styles = StyleSheet.create({
+  containerSearch: {
+    position: 'absolute',
+    top: 20,
+    width: Dimensions.get('screen').width,
+  },
+  boxTitle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  title: {
+    color: '#fff',
+    fontSize: 30,
+    fontFamily: 'Aesthet Nova Black',
+  },
+  containerBoxSearch: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 15,
+    height: 40,
+    marginHorizontal: 20,
+  },
+  boxSearch: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    flex: 1,
+    marginRight: 20,
+  },
+  search: {
+    marginLeft: 5,
+    fontFamily: 'Aesthet Nova Regular',
+    flex: 1,
+    height: 40,
+    color: '#656565',
+  },
+  boxCamera: {
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+  },
+});
 
 export default RenderSearch;
