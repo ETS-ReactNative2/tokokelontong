db15e738b1158bf245b4e85d6d092d4c20907738
andikscript <andikscript@gmail.com>
2021-12-17 20:54:47 +0700

    add file colors

---
+++ constants/colors.js
@@ -0,0 +1,10 @@
+const COLORS = {
+  grannySmithApple: '#ACD792',
+  forestGreenCrayolan: '#61A56C',
+  seaGreen: '#3C8C59',
+  darkSpringGreen: '#167346',
+  white: '#FFFFFF',
+  dimGray: '#656565',
+};
+
+export default COLORS;
