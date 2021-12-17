2cbb9e89b590aa2a5e8e64de3102d16ef2e19f52
andikscript <andikscript@gmail.com>
2021-12-17 18:47:31 +0700

    add image path for clean code

---
+++ constants/image.js
@@ -0,0 +1,35 @@
+const logosplash = require('../assets/image/logosplash.png');
+const coffe = require('../assets/image/home/coffe.png');
+const coffemaker = require('../assets/image/home/coffemaker.png');
+const coffpowder = require('../assets/image/home/coffepowder.png');
+const croissant = require('../assets/image/home/croissant.png');
+const harioskerton = require('../assets/image/home/harioskerton.png');
+const longteapot = require('../assets/image/home/longteapot.png');
+const medical = require('../assets/image/home/medical.png');
+const payment = require('../assets/image/home/payment.png');
+const pudding = require('../assets/image/home/pudding.png');
+const salad = require('../assets/image/home/salad.png');
+const snack = require('../assets/image/home/snack.png');
+const stationery = require('../assets/image/home/stationery.png');
+const teapot = require('../assets/image/home/teapot.png');
+const tiramisu = require('../assets/image/home/tiramisu.png');
+const veggie = require('../assets/image/home/veggie.png');
+
+export default {
+    logosplash,
+    coffe,
+    coffemaker,
+    coffpowder,
+    croissant,
+    harioskerton,
+    longteapot,
+    medical,
+    payment,
+    pudding,
+    salad,
+    snack,
+    stationery,
+    teapot,
+    tiramisu,
+    veggie
+}
