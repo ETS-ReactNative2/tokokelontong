beeb4ca411d5d46bee4647029dc940ce97e2f5f4
andikscript <andikscript@gmail.com>
2021-12-19 21:49:15 +0700

    add detail recipe on navigation

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import { DetailOrders } from './components/Orders';
 import { SplashScreen } from './components/Splash';
 import { BottomTab } from './components/Navigation';
+import { DetailRecipe } from './components/Recipe/Detail';
 
 const Stack = createNativeStackNavigator();
 
@@ -17,6 +18,7 @@ const App = () => {
         <Stack.Screen name="Splash" component={SplashScreen} />
         <Stack.Screen name="BottomTab" component={BottomTab} />
         <Stack.Screen name="DetailOrders" component={DetailOrders} />
+        <Stack.Screen name="DetailRecipe" component={DetailRecipe} />
       </Stack.Navigator>
     </NavigationContainer>
   );
