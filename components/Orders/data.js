81b362d6bc3221ef8eb38eb58d29137ef71d573c
andikscript <andikscript@gmail.com>
2021-12-19 21:01:24 +0700

    refactor data json from Detail Orders

---
+++ components/Orders/data.js
@@ -0,0 +1,26 @@
+const data = [
+  {
+    time: '09.00',
+    title: 'Minggu, 05 Desember 2021',
+    description: 'Pesanan anda di jemput kurir',
+  },
+  {
+    time: '10.00',
+    title: 'Minggu, 05 Desember 2021',
+    description: 'Pesanan anda dalam perjalanan',
+  },
+  {
+    time: '11.00',
+    title: 'Minggu, 05 Desember 2021',
+    description: 'Pesanan anda sampai tujuan anda',
+  },
+  {
+    time: '12.00',
+    title: 'Minggu, 05 Desember 2021',
+    description: 'Pesanan telah diterima',
+  },
+];
+
+export {
+    data
+}
