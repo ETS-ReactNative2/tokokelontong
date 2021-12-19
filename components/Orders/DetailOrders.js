98f9c4a60f15924ed5b24b35c36c770d4f3ab345
andikscript <andikscript@gmail.com>
2021-12-19 21:00:39 +0700

    clean code style and refactor data json

--- components/Orders/DetailOrders.js
+++ components/Orders/DetailOrders.js
@@ -1,107 +1,73 @@
 import React from 'react';
 import {
+  StyleSheet,
   Text,
   View,
 } from 'react-native';
 import { colors } from '../../src/config/colors';
 import Timeline from 'react-native-timeline-flatlist';
-
-const data = [
-  {
-    time: '09.00',
-    title: 'Minggu, 05 Desember 2021',
-    description: 'Pesanan anda di jemput kurir'
-  },
-  {
-    time: '10.00',
-    title: 'Minggu, 05 Desember 2021',
-    description: 'Pesanan anda dalam perjalanan'
-  },
-  {
-    time: '11.00',
-    title: 'Minggu, 05 Desember 2021',
-    description: 'Pesanan anda sampai tujuan anda'
-  },
-  {
-    time: '12.00',
-    title: 'Minggu, 05 Desember 2021',
-    description: 'Pesanan telah diterima'
-  },
-]
+import { Header } from '..';
+import { data } from '.';
 
 const DetailOrders = () => { 
     return (
-      <View
-        style={{
-          flex: 1,
-          backgroundColor: colors.white,
-        }}>
-        <View
-          style={{
-            backgroundColor: colors.forestGreenCrayolan,
-            height: 50,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <Text
-            style={{
-              fontSize: 19,
-              fontFamily: 'Aesthet Nova Regular',
-              color: colors.white,
-            }}>
-            Detail Orders
-          </Text>
-        </View>
-        <View
-          style={{
-            flex: 1,
-            marginTop: 10,
-            marginHorizontal: 10,
-          }}>
-          <Text
-            style={{
-              fontSize: 19,
-              fontFamily: 'Aesthet Nova Regular',
-              color: colors.darkSpringGreen,
-              marginBottom: 20,
-            }}>
-            Status pesanan Anda
-          </Text>
-          <View
-            style={{
-              flex: 1,
-            }}>
+      <View style={styles.container}>
+        <Header text={'Detail Orders'} />
+        <View style={styles.containerTimeline}>
+          <Text style={styles.textTitle}> Status pesanan Anda</Text>
+          <View style={{flex: 1}}>
             <Timeline
               data={data}
               innerCircle={'dot'}
               lineColor={colors.forestGreenCrayolan}
               circleColor={colors.forestGreenCrayolan}
-              timeStyle={{
-                backgroundColor: '#ff9797',
-                textAlign: 'center',
-                color: colors.white,
-                paddingHorizontal: 2,
-                paddingVertical: 3,
-                fontFamily: 'Aesthet Nova Regular',
-                borderRadius: 8,
-                fontSize: 12,
-              }}
-              descriptionStyle={{
-                borderBottomWidth: 0.5,
-                paddingBottom: 15,
-                fontFamily: 'Aesthet Nova Regular',
-                color: colors.darkSpringGreen,
-                borderBottomColor: colors.forestGreenCrayolan,
-              }}
-              titleStyle={{
-                color: colors.dimGray,
-                fontFamily: 'Aesthet Nova Regular',
-              }}
+              timeStyle={styles.timeline}
+              descriptionStyle={styles.timelineDesc}
+              titleStyle={styles.titleTimeline}
             />
           </View>
         </View>
       </View>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.white,
+  },
+  containerTimeline: {
+    flex: 1,
+    marginTop: 10,
+    marginHorizontal: 10,
+  },
+  textTitle: {
+    fontSize: 19,
+    fontFamily: 'Aesthet Nova Regular',
+    color: colors.darkSpringGreen,
+    marginBottom: 20,
+  },
+  timeline: {
+    backgroundColor: '#ff9797',
+    textAlign: 'center',
+    color: colors.white,
+    paddingHorizontal: 2,
+    paddingVertical: 3,
+    fontFamily: 'Aesthet Nova Regular',
+    borderRadius: 8,
+    fontSize: 12,
+  },
+  timelineDesc: {
+    borderBottomWidth: 0.5,
+    paddingBottom: 15,
+    fontFamily: 'Aesthet Nova Regular',
+    color: colors.darkSpringGreen,
+    borderBottomColor: colors.forestGreenCrayolan,
+  },
+  titleTimeline: {
+    color: colors.dimGray,
+    fontFamily: 'Aesthet Nova Regular',
+  },
+});
 
 export default DetailOrders;
