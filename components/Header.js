4d20485283fbad9f962b7ae6c0030e0c96d39030
andikscript <andikscript@gmail.com>
2021-12-19 22:11:42 +0700

    clean code import

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {colors} from '../src/config/colors';
+import { StyleSheet, Text, View } from 'react-native';
+import { colors } from '../src/config/colors';
 
 const Header = ({text}) => {
   return (
