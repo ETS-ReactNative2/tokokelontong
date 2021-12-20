a6238279fe0406c80b8130d7979dd18d3881dfea
andikscript <andikscript@gmail.com>
2021-12-20 20:36:26 +0700

    add palette

--- constants/colors.js
+++ constants/colors.js
@@ -5,6 +5,12 @@ const COLORS = {
   darkSpringGreen: '#167346',
   white: '#FFFFFF',
   dimGray: '#656565',
+  red: 'rgb(255,0,0)',
+  black: '#000000',
+  culturedE: '#eeeeee',
+  culturedF: '#fafafa',
+  forestGreenTraditional: '#0a4823',
+  lightCoral: '#ff9797',
 };
 
 export default COLORS;
